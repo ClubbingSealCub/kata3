95680e013e5ab3567026c80f1b2882658c815c2f
Daniel <Daniel@Prtatil>
2014-10-13 13:18:55 +0100

    Viewer y Reader

---
+++ src/kata3/HistogramViewer.java
@@ -0,0 +1,7 @@
+package kata3;
+
+import kata3.Kata3.Histogram;
+
+public class HistogramViewer <Type>{
+    public void print(Histogram<Type> histogram){}; 
+}
---
+++ src/kata3/MailReader.java
@@ -0,0 +1,18 @@
+package kata3;
+
+public class MailReader {
+
+    private final String filepath;
+
+    public MailReader(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public String[] readDomains() {
+        return null;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+}
