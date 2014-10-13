27ca31f676c0631edc42e5734c9ac9cc7ea3987c
Daniel <Daniel@Prtatil>
2014-10-13 14:08:37 +0100

    Finalizado

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,33 +1,12 @@
 package kata3;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Kata3 {
 
-    public class Histogram<Type> extends HashMap<Type, Integer> {
-    }
-
-    public Map.Entry<Object, Integer> maxEntry(Object[] vFuente) {
-        Map<Object, Integer> mapa = new HashMap() {
-        };
-        for (Object vFuente1 : vFuente) {
-            if (mapa.containsKey(vFuente1)) {
-                mapa.put(vFuente1, mapa.get(vFuente1) + 1);
-            } else {
-                mapa.put(vFuente1, 1);
-            }
-        }
-        Map.Entry<Object, Integer> maxEntry = null;
-        for (Map.Entry<Object, Integer> entry : mapa.entrySet()) {
-            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
-                maxEntry = entry;
-            }
-        }
-        return maxEntry;
-    }
-
     public static void main(String[] args) {
+        MailReader mailReader = new MailReader("mails.txt");
+        HistogramBuilder<String> builder = new HistogramBuilder();
+        Histogram<String> histogram = builder.build(mailReader.readDomains());
+        new HistogramViewer<String>().print(histogram);
 
     }
 
