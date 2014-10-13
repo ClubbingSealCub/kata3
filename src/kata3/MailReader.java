5043309eeccc97a3ca8a86429538f3a4c4197cdf
Daniel <Daniel@Prtatil>
2014-10-13 13:24:00 +0100

    try-catch on mailreader

--- src/kata3/MailReader.java
+++ src/kata3/MailReader.java
@@ -1,18 +1,37 @@
 package kata3;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MailReader {
 
-    private final String filepath;
+    private final String filePath;
 
-    public MailReader(String filepath) {
-        this.filepath = filepath;
+    public MailReader(String filePath) {
+        this.filePath = filePath;
     }
 
     public String[] readDomains() {
-        return null;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+            return null;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MailReader.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException ex) {
+                Logger.getLogger(MailReader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     public String getFilepath() {
-        return filepath;
+        return filePath;
     }
 }
